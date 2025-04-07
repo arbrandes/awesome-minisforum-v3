d0fa8114917dac7b85d3f35451992f1d91b029ae
Adolfo R. Brandes <arbrandes@arbrand.es>
2025-04-07 09:53:07 -0300

    Power adjust for Debian 12

--- scripts/ryzenadj_power_profiles/power_adjust.sh
+++ scripts/ryzenadj_power_profiles/power_adjust.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-RYZENADJ="/path/to/ryzenadj"
+RYZENADJ="/usr/local/bin/ryzenadj"
 INTERVAL=5
 LASTKNOWN=""
+MONITOR=eDP-1
+PERFORMANCE_MW=37000
+PERFORMANCE_MODE=2560x1600@165.000
+BALANCED_MW=22000
+BALANCED_MODE=2560x1600@60.000
+POWERSAVE_MW=15000
+POWERSAVE_MODE=2560x1600@60.000
 
 while :
 do
@@ -13,19 +20,24 @@ do
 
 		if [ "$CURPP" == "performance" ]; then
 			echo "performance"
-			sudo $RYZENADJ --stapm-limit=28000 --fast-limit=28000 --slow-limit=28000 --apu-slow-limit=28000
-			kscreen-doctor output.1.mode.0
+			sudo $RYZENADJ --stapm-limit=$PERFORMANCE_MW --fast-limit=$PERFORMANCE_MW --slow-limit=$PERFORMANCE_MW --apu-slow-limit=$PERFORMANCE_MW
+			gnome-randr modify --mode $PERFORMANCE_MODE $MONITOR
 		elif [ "$CURPP" == "power-saver" ]; then
 			echo "power-save"
 			sudo $RYZENADJ --stapm-limit=15000 --fast-limit=15000 --slow-limit=15000 --apu-slow-limit=15000
-			kscreen-doctor output.1.mode.1
+			gnome-randr modify --mode $POWERSAVE_MODE $MONITOR
 		else
-			echo "balanced"
-			sudo $RYZENADJ --stapm-limit=22000 --fast-limit=22000 --slow-limit=22000 --apu-slow-limit=22000
-			kscreen-doctor output.1.mode.1
+			if on_ac_power; then
+				echo "performance"
+				sudo $RYZENADJ --stapm-limit=$PERFORMANCE_MW --fast-limit=$PERFORMANCE_MW --slow-limit=$PERFORMANCE_MW --apu-slow-limit=$PERFORMANCE_MW
+				gnome-randr modify --mode $PERFORMANCE_MODE $MONITOR
+			else
+				echo "balanced"
+				sudo $RYZENADJ --stapm-limit=22000 --fast-limit=22000 --slow-limit=22000 --apu-slow-limit=22000
+				gnome-randr modify --mode $BALANCED_MODE $MONITOR
+			fi
 		fi
 	fi
 
 	sleep $INTERVAL
 done
-
